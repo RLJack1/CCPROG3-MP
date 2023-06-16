8adf6ebe484af7b0cd447d367826fde045cf6001
RLJack1 <133231806+RLJack1@users.noreply.github.com>
2023-06-16 11:14:26 +0800

    Item.java
    Item.v1 (6/16, 11:14)

--- Item.java
+++ Item.java
@@ -1,1 +1,33 @@
-//class parameters
+import java.lang.String;
+
+public class Item {
+    private String name;
+    private double calories;
+    private boolean standalone = false;
+    public double price;
+    public int stock;
+
+    public String getName() {
+        return name;
+    }
+
+    public double getCalories() {
+        return calories;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+}
