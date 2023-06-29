9b344102c09d79b44bb6ab067dac079c3c2c6e82
RLJack1 <renzo_bautista@dlsu.edu.ph>
2023-06-29 10:52:40 +0800

    PD Update, POD string input solution committed

--- ProductDisplay.java
+++ ProductDisplay.java
@@ -100,48 +100,17 @@ public class ProductDisplay {
         System.out.println("Items on Sale:\tName\t\t Calories\tPrice\tstock");
         Item toBuy = new Item(null, 0, false, 0, 0);
         boolean done = false;
-<<<<<<< HEAD
-=======
-		int index = 0;
-		int input = 99;
->>>>>>> 6c466fd0cc56f8d944b542e339bcf8c694d4bb79
         
-		//display items 
         for(Item item : itemList) 
-        {	
-			if(item.getStock() != 0 && ((item.getName().length() >= 12 && index > 9) || item.getName().length() >= 13)) {
-				System.out.println("\t\t" + (index + 1) + ": " + item.getName() + "\t" + item.getCalories() + "\t" + item.getPrice() + "\t" + item.getStock());
-			}
-			
-			else if (item.getStock() != 0) {
-				System.out.println("\t\t" + (index + 1) + ": " + item.getName() + "\t\t" + item.getCalories() + "\t" + item.getPrice() + "\t" + item.getStock());
-			}
-            
-			else
-                System.out.println("\t\t" + (index + 1) + ": " + item.getName() + " is SOLD OUT.");
-			
-            index++;
+        {
+            if(item.getStock() != 0)
+                System.out.println("\t\t" + item.getName() + "\t"  + item.getCalories() + "\t" + item.getPrice()  + "\t" + item.getStock());
+            else
+                System.out.println("\t\t " + item.getName() + "is SOLD OUT.");
         }
-		
-		//take input 
-		do {
-            System.out.print("Input the index of the item: ");
-            input = s.nextInt();
-			
-            if(input >= 0 && input <= itemList.size()) {
-				toBuy = itemList.get(input - 1);
-				System.out.println("Successfully selected " + toBuy.getName() + "!\n");
-				done = true;
-			}
-			
-			else {
-				System.out.println("Invalid input. Please try again.\n");
-			}
-			
-        } while (done == false);
 
-<<<<<<< HEAD
         do {
+            System.out.println("Enter the name of your item: ");
             toBuy = userChoice(itemList, s);
 
             if (toBuy != null) {
@@ -152,8 +121,5 @@ public class ProductDisplay {
         } while (!done);
 
         return toBuy;
-=======
-		return toBuy;
->>>>>>> 6c466fd0cc56f8d944b542e339bcf8c694d4bb79
     }
 }
